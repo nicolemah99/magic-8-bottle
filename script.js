370c7d7e2b03627641b85c15c9a6413940a1b5e0
Nicole Mah <mnmah@alaska.edu>
2023-02-08 11:34:18 -0900

    testing data change

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then((response) => response.json())
-    .then((data) => console.log(data));
-
-const element = document.getElementById("test");
-element.style.backgroundColor = "#00FF00";
-  
+    .then((data) => console.log(data))
+    .then(document.getElementById("test").innerHTML = data
+    );
