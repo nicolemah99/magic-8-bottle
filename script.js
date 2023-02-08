cfd560431d58313cdead812954067dafbcb483a1
Nicole Mah <mnmah@alaska.edu>
2023-02-08 11:40:40 -0900

    change to hello

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then((response) => response.json())
     .then((data) => console.log(data))
-    .then(document.getElementById("test").innerHTML = data
-    );
+    .then(document.getElementById("test").innerHTML = "hello");
 
 });
