ab1ee06779f913c2ff274daa2d369d0f4f0b912a
Nicole Mah <mnmah@alaska.edu>
2023-02-08 11:24:16 -0900

    testing background color

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then((response) => response.json())
     .then((data) => console.log(data));
-  
+
+$(document).ready(function() {const element = document.getElementById("test");
+element.style.backgroundColor = "#00FF00";
+});
