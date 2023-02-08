0696027aed73cd73656ec9179e6ff01f04d1c1d0
Nicole Mah <mnmah@alaska.edu>
2023-02-08 11:38:41 -0900

    add dom content loaded

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then((response) => response.json())
     .then((data) => console.log(data))
     .then(document.getElementById("test").innerHTML = data
     );
+
+});
