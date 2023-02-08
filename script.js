43824442e24453dceb775397797dfd0600c89538
Nicole Mah <mnmah@alaska.edu>
2023-02-08 11:08:41 -0900

    successfully fetch data from github json

--- script.js
+++ script.js
@@ -1,44 +1,4 @@
-function search() {
-    var queryURL = "https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json";
-
-    fetch(queryURL)
-            .then(function (response) {
-                // response.json() returns a json string,
-                // returning it will convert it 
-                // to a pure JavaScript 
-                // object for the next then's callback
-                return response.json();
-            })
-            .then(function (users) {
-                // users is a JavaScript object here
-                displayUsersAsATable(users);
-            })
-            .catch(function (error) {
-                console.log('Error during fetch: ' + error.message);
-            });
-}
-
-function displayUsersAsATable(users) {
-    // users is a JavaScript object
-
-    // empty the div that contains the results
-    var usersDiv = document.querySelector("#users");
-    usersDiv.innerHTML = "";
-
-    // creates and populate the table with users
-    var table = document.createElement("table");
-
-    // iterate on the array of users
-    users.forEach(function (currentUser) {
-        // creates a row
-        var row = table.insertRow();
-        // insert cells in the row
-        var nameCell = row.insertCell();
-        nameCell.innerHTML = currentUser.name;
-        var cityCell = row.insertCell();
-        cityCell.innerHTML = currentUser.address.city;
-    });
-
-    // adds the table to the div
-    usersDiv.appendChild(table);
-}
+fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  
