0f6c54489bf1e3afbc8bfaa237bf2b6961428b8f
Nicole Mah <mnmah@alaska.edu>
2023-02-08 12:30:43 -0900

    hide start button

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     initialize()
-    document.getElementById("WineBottle").src = "https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/wineBottle.svg";
 
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then((response) => response.json())
-    .then((data) => console.log(data))
-    .then(document.getElementById("test").style.color = 'red');
+    .then((data) => console.log(data));
 
 });
 
 function initialize(){
-    document.getElementById("WineBottle").src = "https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/wineBottle.svg";
-    document.getElementById('Title').innerHTML = "Let the Magic 8 Bottle Choose An Experience for you!";
+    const start = document.getElementById("StartButton");
+    start.onclick = startGame(start);
+}
+
+function startGame(startButton){
+    startButton.style.display = "none";
 }
