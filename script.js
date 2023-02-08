7a8e2fbf449a79621f6f83b9e92d15e9ca829eaa
Nicole Mah <mnmah@alaska.edu>
2023-02-08 12:11:17 -0900

    initialize function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    initialize()
     document.getElementById("WineBottle").src = "https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/wineBottle.svg";
 
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
@@ -7,3 +8,8 @@ fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then(document.getElementById("test").style.color = 'red');
 
 });
+
+function initialize(){
+    document.getElementById("WineBottle").src = "https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/wineBottle.svg";
+    document.getElementById('Title').innerHTML = "Let the Magic 8 Bottle Choose An Experience for you!";
+}
