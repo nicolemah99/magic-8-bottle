013c1f8c93b6924556fe82e7c4f6f2d7c6871f17
Nicole Mah <mnmah@alaska.edu>
2023-02-08 11:43:24 -0900

    red

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
     .then((response) => response.json())
     .then((data) => console.log(data))
-    .then(document.getElementById("test").innerHTML = "hello");
+    .then(document.getElementById("test").style.color = 'red');
 
 });
