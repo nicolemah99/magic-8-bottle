5090a48877a480d5d441832f2c410ffceab823d2
Nicole Mah <mnmah@alaska.edu>
2023-02-09 14:07:35 -0900

    question flow to Number 4

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-
-fetch('https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-
+document.addEventListener("DOMContentLoaded", function () {
+	fetch("https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/testData.json")
+		.then((response) => response.json())
+		.then((data) => console.log(data));
 });
 
+function startGame() {
+	const startButton = document.getElementById("StartButton");
+	startButton.style.display = "none";
+	nextQuestion(1);
+}
 
-function startGame(){
-    const startButton = document.getElementById("StartButton");
-    startButton.style.display = "none";
+function nextQuestion(number) {
+    const prevNum = number -1;
+
+    if (number != 1){
+        let stringNum = prevNum.toString();
+        const questionDiv = document.getElementById(`Question${stringNum}`);
+        questionDiv.style.display = 'none';
+    } 
+
+    let stringNum = number.toString();
+	const questionDiv = document.getElementById(`Question${stringNum}`);
+    questionDiv.style.display = 'flex';
+    
 }
