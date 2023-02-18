754597f02f5f2c2b805a82e7e3ce73b0f0a2ee0f
Nicole Mah <nicolemah99@gmail.com>
2023-02-17 21:58:06 -0900

    getting answers

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {});
+const answers = []
+
+//Listen for answers when buttons pushed
+document.addEventListener("DOMContentLoaded", () => {
+	const buttons = document.getElementsByClassName("answer");
+
+	const buttonPressed = (e) => {
+        answers.push(e.target.innerHTML);
+	};
+
+	for (let button of buttons) {
+		button.addEventListener("click", buttonPressed);
+	}
+});
 
 function getJSON() {
 	fetch(
-		'https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/experiences.json'
+		"https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/experiences.json"
 	)
 		.then(function (response) {
 			return response.json();
@@ -26,8 +39,8 @@ function getRandExperience(exp) {
 }
 
 function randExp() {
-    hideElement('StartBtn');
-    hideElement('RandBtn');
+	hideElement("StartBtn");
+	hideElement("RandBtn");
 	shake();
 }
 
@@ -40,73 +53,76 @@ function getRandomInt(min, max) {
 //Helper Functions
 function hideElement(id) {
 	const element = document.getElementById(id);
-	element.style.display = 'none';
+	element.style.display = "none";
 }
 
-function showElement(id, type='block') {
-    const element = document.getElementById(id);
-    element.style.display = type;
+function showElement(id, type = "block") {
+	const element = document.getElementById(id);
+	element.style.display = type;
 }
 
 function updateProgressBar(number) {
-	let bar = document.getElementById('progressBarFull');
+	let bar = document.getElementById("progressBarFull");
 	bar.style.width = `${((number - 1) / 4) * 100}%`;
 }
 
 function shake() {
-	document.getElementById('wineBottle').className = 'shake';
+	document.getElementById("wineBottle").className = "shake";
 	setTimeout(finishGame, 1000);
 }
 
 function showExperience(exp) {
-	const link = document.getElementById('Experience');
-	const name = document.getElementById('Name');
-	const img = document.getElementById('ExpImg');
-	link.href = exp['link'];
-	name.innerHTML = exp['name'];
-	img.src = exp['img'];
-	showElement('ExpImg');
-    showElement('ExperienceDiv');
-    showElement('StartOverBtn');
+	const link = document.getElementById("Experience");
+	const name = document.getElementById("Name");
+	const img = document.getElementById("ExpImg");
+	link.href = exp["link"];
+	name.innerHTML = exp["name"];
+	img.src = exp["img"];
+	showElement("ExpImg");
+	showElement("ExperienceDiv");
+	showElement("StartOverBtn");
 }
 
 function startGame() {
-	showElement('progressBar','flex')
-	hideElement('StartBtn');
-    hideElement('RandBtn');
-    hideElement('ExperienceDiv');
+	showElement("progressBar", "flex");
+	hideElement("StartBtn");
+	hideElement("RandBtn");
+	hideElement("ExperienceDiv");
 	nextQuestion(1);
 }
 
-function nextQuestion(number) {
+function nextQuestion(number, button) {
 	const prevNum = number - 1;
 
 	if (number != 1) {
 		let stringNum = prevNum.toString();
-        hideElement(`Question${stringNum}`);
+		hideElement(`Question${stringNum}`);
 	}
 
 	updateProgressBar(number);
 
 	let stringNum = number.toString();
 	const questionDiv = document.getElementById(`Question${stringNum}`);
-	questionDiv.style.display = 'flex';
-	questionDiv.style.flexDirection = 'column';
+	questionDiv.style.display = "flex";
+	questionDiv.style.flexDirection = "column";
 }
 
 function finishGame() {
-    hideElement('progressBar');
-    hideElement('Question4');
-	showElement('StartOverBtn');
+    console.log(answers);
+	hideElement("progressBar");
+	hideElement("Question4");
+	showElement("StartOverBtn");
 	getJSON();
 }
 
 function startOver() {
-	const wineBottle = document.getElementById('wineBottle');
-	wineBottle.classList.remove('shake');
-
-    hideElement('ExperienceDiv');
-    hideElement('StartOverBtn');
-    showElement('StartBtn');
-    showElement('RandBtn');
+	const wineBottle = document.getElementById("wineBottle");
+	wineBottle.classList.remove("shake");
+    while(answers.length > 0) {
+        answers.pop();
+    }
+	hideElement("ExperienceDiv");
+	hideElement("StartOverBtn");
+	showElement("StartBtn");
+	showElement("RandBtn");
 }
