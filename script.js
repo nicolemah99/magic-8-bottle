65eed14249d4f6ada559470865e0cbc8e6cbabd8
Nicole Mah <nicolemah99@gmail.com>
2023-02-17 22:17:29 -0900

    Hiding Wine Bottle

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const answers = []
 
 //Listen for answers when buttons pushed
-document.addEventListener("DOMContentLoaded", () => {
-	const buttons = document.getElementsByClassName("answer");
+document.addEventListener('DOMContentLoaded', () => {
+	const buttons = document.getElementsByClassName('answer');
 
 	const buttonPressed = (e) => {
         answers.push(e.target.innerHTML);
 	};
 
 	for (let button of buttons) {
-		button.addEventListener("click", buttonPressed);
+		button.addEventListener('click', buttonPressed);
 	}
 });
 
 function getJSON() {
 	fetch(
-		"https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/experiences.json"
+		'https://cdn.jsdelivr.net/gh/nicolemah99/magic-8-bottle/experiences.json'
 	)
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (exp) {
-			getRandExperience(exp);
+			getRandExp(exp);
 		})
 		.catch(function (error) {
 			console.error(
@@ -30,17 +30,17 @@ function getJSON() {
 		});
 }
 
-//Return random Experience from JSON file
-function getRandExperience(exp) {
+//Return random Exp from JSON file
+function getRandExp(exp) {
 	var len = Object.keys(exp).length;
 	var randInt = getRandomInt(0, len);
 	const chosen = exp[randInt];
-	showExperience(chosen);
+	showExp(chosen);
 }
 
 function randExp() {
-	hideElement("StartBtn");
-	hideElement("RandBtn");
+	hideElement('StartBtn');
+	hideElement('RandBtn');
 	shake();
 }
 
@@ -53,41 +53,42 @@ function getRandomInt(min, max) {
 //Helper Functions
 function hideElement(id) {
 	const element = document.getElementById(id);
-	element.style.display = "none";
+	element.style.display = 'none';
 }
 
-function showElement(id, type = "block") {
+function showElement(id, type = 'block') {
 	const element = document.getElementById(id);
 	element.style.display = type;
 }
 
 function updateProgressBar(number) {
-	let bar = document.getElementById("progressBarFull");
+	let bar = document.getElementById('progressBarFull');
 	bar.style.width = `${((number - 1) / 4) * 100}%`;
 }
 
 function shake() {
-	document.getElementById("wineBottle").className = "shake";
+	document.getElementById('wineBottle').className = 'shake';
 	setTimeout(finishGame, 1000);
 }
 
-function showExperience(exp) {
-	const link = document.getElementById("Experience");
-	const name = document.getElementById("Name");
-	const img = document.getElementById("ExpImg");
-	link.href = exp["link"];
-	name.innerHTML = exp["name"];
-	img.src = exp["img"];
-	showElement("ExpImg");
-	showElement("ExperienceDiv");
-	showElement("StartOverBtn");
+function showExp(exp) {
+	const link = document.getElementById('Exp');
+	const name = document.getElementById('Name');
+	const img = document.getElementById('ExpImg');
+	link.href = exp['link'];
+	name.innerHTML = exp['name'];
+	img.src = exp['img'];
+    hideElement('WineBottleDiv');
+	showElement('ExpImg');
+	showElement('ExpDiv');
+	showElement('StartOverBtn');
 }
 
 function startGame() {
-	showElement("progressBar", "flex");
-	hideElement("StartBtn");
-	hideElement("RandBtn");
-	hideElement("ExperienceDiv");
+	showElement('progressBar', 'flex');
+	hideElement('StartBtn');
+	hideElement('RandBtn');
+	hideElement('ExpDiv');
 	nextQuestion(1);
 }
 
@@ -103,26 +104,29 @@ function nextQuestion(number, button) {
 
 	let stringNum = number.toString();
 	const questionDiv = document.getElementById(`Question${stringNum}`);
-	questionDiv.style.display = "flex";
-	questionDiv.style.flexDirection = "column";
+	questionDiv.style.display = 'flex';
+	questionDiv.style.flexDirection = 'column';
 }
 
 function finishGame() {
     console.log(answers);
-	hideElement("progressBar");
-	hideElement("Question4");
-	showElement("StartOverBtn");
+	hideElement('progressBar');
+	hideElement('Question4');
+	showElement('StartOverBtn');
 	getJSON();
 }
 
 function startOver() {
-	const wineBottle = document.getElementById("wineBottle");
-	wineBottle.classList.remove("shake");
+	const wineBottle = document.getElementById('wineBottle');
+	wineBottle.classList.remove('shake');
+
+    //Clear global answers array
     while(answers.length > 0) {
         answers.pop();
     }
-	hideElement("ExperienceDiv");
-	hideElement("StartOverBtn");
-	showElement("StartBtn");
-	showElement("RandBtn");
+	hideElement('ExpDiv');
+	hideElement('StartOverBtn');
+    showElement('WineBottleDiv');
+	showElement('StartBtn');
+	showElement('RandBtn');
 }
